9014fc5dd2c781aa5d130c1fc53f88dea2c381c1
Asari Mitsuteru <asari.mtr@gmail.com>
2013-08-31 22:59:01 +0900

    implemented sample

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,21 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.iteratee.{Enumerator, Iteratee}
 
 object Application extends Controller {
   
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  def line = WebSocket.using[String] { request =>
+    val in = Iteratee.foreach[String](println).mapDone { _ =>
+      println("Disconnected")
+    }
+
+    val out = Enumerator[String]("Hello!")
+
+    (in, out)
+  }
 }
