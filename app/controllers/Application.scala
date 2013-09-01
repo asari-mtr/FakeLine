83d75f3a1b9752b8d67cac2f6ae52f8ae6a02fe0
Asari Mitsuteru <asari.mtr@gmail.com>
2013-09-01 18:33:00 +0900

    implement broadcast

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,7 +2,7 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import play.api.libs.iteratee.{Enumerator, Iteratee}
+import play.api.libs.iteratee.{Concurrent, Enumerator, Iteratee}
 import play.api.libs.json.{Json, JsValue}
 
 object Application extends Controller {
@@ -11,19 +11,21 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  val (enumerator, channel) = Concurrent.broadcast[JsValue]
+
   def line = WebSocket.using[JsValue] { request =>
     val in = Iteratee.foreach[JsValue]{ event =>
-      println((event \ "message").as[String])
+      channel.push(event)
     }.mapDone { _ =>
       println("Disconnected")
     }
 
-    val out = Enumerator[JsValue](Json.toJson(
-      Map(
-        "response" -> Json.toJson("Hello")
-      )
-    ))
+//    val out = Enumerator[JsValue](Json.toJson(
+//      Map(
+//        "response" -> Json.toJson("Hello")
+//      )
+//    ))
 
-    (in, out)
+    (in, enumerator)
   }
 }
