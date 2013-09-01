9eda731c64b9648aa241dc6989af860d08c8bdb3
Asari Mitsuteru <asari.mtr@gmail.com>
2013-09-01 17:26:22 +0900

    implement json

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,7 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.iteratee.{Enumerator, Iteratee}
+import play.api.libs.json.{Json, JsValue}
 
 object Application extends Controller {
   
@@ -10,12 +11,18 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def line = WebSocket.using[String] { request =>
-    val in = Iteratee.foreach[String](println).mapDone { _ =>
+  def line = WebSocket.using[JsValue] { request =>
+    val in = Iteratee.foreach[JsValue]{ event =>
+      println((event \ "message").as[String])
+    }.mapDone { _ =>
       println("Disconnected")
     }
 
-    val out = Enumerator[String]("Hello!")
+    val out = Enumerator[JsValue](Json.toJson(
+      Map(
+        "response" -> Json.toJson("Hello")
+      )
+    ))
 
     (in, out)
   }
